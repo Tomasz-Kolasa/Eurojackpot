d08a8eea7b7bc7d88045daecc58053a2907ea2c1
Tom <kasky@wp.pl>
2022-05-19 11:16:49 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eurojackpot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
